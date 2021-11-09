c47402c1f66f7e81469b3b2af9c698a774923835
David Assefa Tofu <davidat@bu.edu>
2021-11-09 12:04:10 +0300

    project structure added

--- src/api.rs
+++ src/api.rs
@@ -108,15 +108,13 @@ impl AddApiKey for RequestBuilder {
     }
 }
 
-// The main Api object we export
-pub struct Api<'a> {
-    api_key: &'a str,
-    client: Client,
-}
-
 // https://github.com/toggl/toggl_api_docs/blob/master/chapters/time_entries.md#time-entries
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct TimeEntry {
+    /// The id field is not necessary when creating a time entry
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub id: Option<i64>,
+
     // strongly suggested to be used
     #[serde(skip_serializing_if = "Option::is_none")]
     pub description: Option<String>,
@@ -164,8 +162,8 @@ pub struct TimeEntry {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub duronly: Option<bool>,
 
-    // ONLY sent in response. I hope this doesn't mess up requests.
-    // Timestamp that is sent in the response, indicates the time item was last update.
+    /// ONLY sent in response. I hope this doesn't mess up requests.
+    /// Timestamp that is sent in the response, indicates the time item was last update.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub at: Option<DateTime<Utc>>,
 }
@@ -182,6 +180,110 @@ pub struct TimeEntryResponse {
     data: TimeEntry,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Workspace {
+    /// The id field is not necessary when creating a workspace
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub id: Option<i64>,
+
+    /// the name of the workspace
+    pub name: String,
+
+    /// If it's a pro workspace or not. Shows if someone is paying for the workspace or not
+    pub premium: bool,
+
+    /// shows whether currently requesting user has admin access to the workspace
+    pub admin: bool,
+
+    /// default hourly rate for workspace, won't be shown to non-admins if the only_admins_see_billable_rates flag is set to true
+    pub default_hourly_rate: f64,
+
+    /// default currency for workspace
+    pub default_currency: String,
+
+    /// whether only the admins can create projects or everybody
+    pub only_admins_may_create_projects: bool,
+
+    /// whether only the admins can see billable rates or everybody
+    pub only_admins_see_billable_rates: bool,
+
+    /// type of rounding
+    pub rounding: i64,
+
+    /// round up to nearest minute
+    pub rounding_minutes: i64,
+
+    /// timestamp that indicates the time workspace was last updated
+    pub at: DateTime<Utc>,
+
+    /// URL pointing to the logo [if set, otherwise omited]
+    pub logo_url: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Tag {
+    /// The id field is not necessary when creating a workspace
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub id: Option<i64>,
+
+    /// The name of the tag (unique in workspace)
+    pub name: String,
+
+    /// workspace ID, where the tag will be used
+    pub wid: i64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Project {
+    /// The name of the project (required, unique for client and workspace)
+    name: String,
+
+    /// workspace ID, where the project will be saved (required)
+    wid: i64,
+
+    /// client ID (not required)
+    cid: i64,
+
+    /// whether the project is archived or not (by default true)
+    active: bool,
+
+    /// whether project is accessible for only project users or for all workspace users (default true)
+    is_private: bool,
+
+    /// whether the project can be used as a template (not required)
+    template: Option<bool>,
+
+    /// id of the template project used on current project's creation
+    template_id: i64,
+
+    /// whether the project is billable or not (default true, available only for pro workspaces)
+    billable: bool,
+
+    /// whether the estimated hours are automatically calculated based on task estimations or manually fixed based on the value of 'estimated_hours' (default false, not required, premium functionality)
+    auto_estimates: Option<bool>,
+
+    /// if auto_estimates is true then the sum of task estimations is returned, otherwise user inserted hours (not required, premium functionality)
+    estimated_hours: Option<i64>,
+
+    /// timestamp that is sent in the response for PUT, indicates the time task was last updated (read-only)
+    at: DateTime<Utc>,
+
+    /// id of the color selected for the project
+    color: String,
+
+    /// hourly rate of the project (not required, premium functionality)
+    rate: Option<f64>,
+
+    /// timestamp indicating when the project was created (UTC time), read-only
+    created_at: DateTime<Utc>,
+}
+
+/// The main Api object
+pub struct Api<'a> {
+    api_key: &'a str,
+    client: Client,
+}
+
 impl<'a> Api<'a> {
     pub fn new(api_key: &'a str) -> Api {
         Api {
@@ -190,17 +292,55 @@ impl<'a> Api<'a> {
         }
     }
 
-    pub fn time_entry_create(&self, time_entry: &TimeEntry) -> ApiResult<TimeEntryResponse> {
-        let endpoint = API_URL.to_owned() + "/time_entries";
+    fn post_and_get_json<BodyJson: Serialize, RespJson: DeserializeOwned>(
+        &self,
+        endpoint: &str,
+        body: &BodyJson,
+    ) -> ApiResult<RespJson> {
         println!("Requesting: {}", endpoint);
         let result = self
             .client
             .post(endpoint)
             .add_api_key(self)
-            .json(&TimeEntryRequest {
+            .json(body)
+            .send_and_get_json::<RespJson>();
+        return result;
+    }
+
+    /// Create a time entry. Look at `TimeEntry`'s documentation for fields that are required.
+    pub fn time_entry_create(&self, time_entry: &TimeEntry) -> ApiResult<TimeEntryResponse> {
+        let endpoint = API_URL.to_owned() + "/time_entries";
+        println!("Requesting: {}", endpoint);
+        let result = self.post_and_get_json(
+            &endpoint,
+            &TimeEntryRequest {
                 time_entry: time_entry.clone(),
-            })
-            .send_and_get_json::<TimeEntryResponse>();
+            },
+        );
+        return result;
+    }
+
+    /// Get workspaces
+    pub fn workspaces_get_all(&self) -> ApiResult<Vec<Workspace>> {
+        let endpoint = API_URL.to_owned() + "/workspaces";
+        println!("Requesting: {}", endpoint);
+        let result = self
+            .client
+            .get(endpoint)
+            .add_api_key(self)
+            .send_and_get_json();
+        return result;
+    }
+
+    /// Get workspace tags
+    pub fn workspaces_tags_all(&self, wid: i64) -> ApiResult<Vec<Tag>> {
+        let endpoint = API_URL.to_owned() + "/workspaces/" + &wid.to_string() + "/tags";
+        println!("Requesting: {}", endpoint);
+        let result = self
+            .client
+            .get(endpoint)
+            .add_api_key(self)
+            .send_and_get_json();
         return result;
     }
 }
